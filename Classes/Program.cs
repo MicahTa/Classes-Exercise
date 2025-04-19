46b27c9b9c9275e1ef0ffa5137f33a55cddccd30
Laptop <micahcorbintanner@gmail.com>
2025-04-19 13:37:55 -0600

    Did the project

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace Classes
+﻿using System.Globalization;
+
+namespace Classes
 {
+
     public class Program
     {
+        /* 
+        Create a new instance of the Car class
+        Set values in the properties for the object
+        Print the values of each property to the Console
+        */
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car parkingLot = new Car();
+            parkingLot.Make = "Ford";
+            parkingLot.Model = "1/2 Ton V8 6.5L";
+            parkingLot.Year = 2007;
+
+            Console.WriteLine(parkingLot.Make);
+            Console.WriteLine(parkingLot.Model);
+            Console.WriteLine(parkingLot.Year);
         }
     }
 }
